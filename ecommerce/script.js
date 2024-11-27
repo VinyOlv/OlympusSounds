d1a6c37f0335abd76912c26843d9db8903de2120
VinyOlv <viny.olliveira16@gmail.com>
2024-11-27 13:03:48 -0300

    aaaaaa

---
+++ ecommerce/script.js
@@ -0,0 +1,8 @@
+//nav bar
+
+const navbar = document.querySelector(".navbar");
+const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener("click", () =>{
+    navbar.classList.toggle("show-menu");
+});
